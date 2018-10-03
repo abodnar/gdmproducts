3cd4c2a5be358a47f7c5fb8961a48f4c7a9cf2a1
Adam Bodnar <ambodnar@gmail.com>
2018-10-02 23:51:48 -0500

    Removing hard coded token

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  API_TOKEN: '"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9zaWQiOiIxNyIsInVuaXF1ZV9uYW1lIjoiaW50ZXJ2aWV3YXBpQGdhcnRuZXIuY29tIiwiZW1haWwiOiJpbnRlcnZpZXdhcGlAZ2FydG5lci5jb20iLCJuYmYiOjE1MzMxNDE2MDUsImV4cCI6MTU5MzE0MTU0NSwiaWF0IjoxNTMzMTQxNjA1fQ.AMhRFClwFUJEsYDI_qbMpQmcX8Z9u0IWvUNX9TWVr7w"',
+  API_TOKEN: '<API TOKEN GOES HERE>"',
   API_URL: '"https://gdm-interview-api.azurewebsites.net/api/v1"'
 }
